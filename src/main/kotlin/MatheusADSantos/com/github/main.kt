06e5be763d5fd127b487920cc6b72b51e253b485
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-28 17:00:00 -0300

    Concatenando duas List<String> e printando com distinct()

--- src/main/kotlin/MatheusADSantos/com/github/main.kt
+++ src/main/kotlin/MatheusADSantos/com/github/main.kt
@@ -1,44 +1,11 @@
 package MatheusADSantos.com.github
 
 fun main() {
-    // Criando uma instância do banco
-    val banco = BancoDeNomes()
-    // Criando uma cópia da instância
-    val nomesSalvos: Collection<String> = banco.nomes
 
-    // Salvando um nome e printando
-    println(banco.nomes)
-    println(nomesSalvos)
-    banco.salva("Matheus")
-    println(banco.nomes)
-    println(nomesSalvos)
-    println(banco.nomes != nomesSalvos)
+    val assistiramCursoKotlin = listOf<String>("Matheus", "Marcela", "Ana", "Carol", "Rafa")
+    val assistiramCursoJava = listOf<String>("Matheus", "Israel", "Daniel")
 
-}
+    val assistiramAmbas = assistiramCursoJava + assistiramCursoKotlin
+    println(assistiramAmbas.distinct())
 
-class BancoDeNomes {
-
-    // Ao usar toList(), estou criando uma cópia 'real' de dados(MutableList) -> nomes(Collection/Imutável),
-    // impedindo assim que consiga alterar os valores da lista mutável dados, através de um cast por ex...
-    val nomes: Collection<String> get() = dados.toList()
-
-    fun salva(nome: String) {
-        dados.add(nome)
-    }
-
-    // Companion object(tornando-a global): para mantes os dados, sem criar nova instancia
-    companion object {
-        private val dados = mutableListOf<String>()
-    }
-
-}
-
-private fun testaColecao() {
-    val nomes: Collection<String> = listOf("Matheus", "Fran", "Gui", "Maria", "Ana")
-    for (nome in nomes) {
-        println(nome)
-    }
-    println(nomes)
-    println("Tem o nome Matheus? ${nomes.contains("Matheus")}")
-    println("Tamanho da coleção ${nomes.size}")
 }
---
+++ src/main/kotlin/MatheusADSantos/com/github/testaColletion.kt
@@ -0,0 +1,44 @@
+package MatheusADSantos.com.github
+
+
+fun testaCopia() {
+    // Criando uma instância do banco
+    val banco = BancoDeNomes()
+    // Criando uma cópia da instância
+    val nomesSalvos: Collection<String> = banco.nomes
+
+    // Salvando um nome e printando
+    println(banco.nomes)
+    println(nomesSalvos)
+    banco.salva("Matheus")
+    println(banco.nomes)
+    println(nomesSalvos)
+    println(banco.nomes != nomesSalvos)
+}
+
+class BancoDeNomes {
+
+    // Ao usar toList(), estou criando uma cópia 'real' de dados(MutableList) -> nomes(Collection/Imutável),
+    // impedindo assim que consiga alterar os valores da lista mutável dados, através de um cast por ex...
+    val nomes: Collection<String> get() = dados.toList()
+
+    fun salva(nome: String) {
+        dados.add(nome)
+    }
+
+    // Companion object(tornando-a global): para mantes os dados, sem criar nova instancia
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+
+}
+
+fun testaColecao() {
+    val nomes: Collection<String> = listOf("Matheus", "Fran", "Gui", "Maria", "Ana")
+    for (nome in nomes) {
+        println(nome)
+    }
+    println(nomes)
+    println("Tem o nome Matheus? ${nomes.contains("Matheus")}")
+    println("Tamanho da coleção ${nomes.size}")
+}
