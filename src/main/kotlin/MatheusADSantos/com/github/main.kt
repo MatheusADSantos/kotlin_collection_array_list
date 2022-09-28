ec5a475774835016b98c6b7cfe23d18ade0f3d60
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-28 09:46:29 -0300

    Criando um BancoDeNomes com val dados do tipo companion object e criando uma cópida em nomes(publica)...

--- src/main/kotlin/MatheusADSantos/com/github/main.kt
+++ src/main/kotlin/MatheusADSantos/com/github/main.kt
@@ -1,7 +1,35 @@
 package MatheusADSantos.com.github
 
 fun main() {
+    // Criando uma instância do banco
+    val banco = BancoDeNomes()
 
+    // Salvando um nome e printando
+    println(banco.nomes)
+    banco.salva("Matheus")
+    println(banco.nomes)
+    println("Aqui garanto que ambas as instancias estão pegando do mesmo banco: ${ banco.nomes == BancoDeNomes().nomes }")
+
+}
+
+class BancoDeNomes {
+
+    // Coleção(nomes) imutável publica, recebendo uma shadow copy(cópia básica),
+    // ou seja, apontando para a mesma referência
+    val nomes: Collection<String> get() = dados
+
+    fun salva(nome: String) {
+        dados.add(nome)
+    }
+
+    // Companion object(tornando-a global): para mantes os dados, sem criar nova instancia
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+
+}
+
+private fun testaColecao() {
     val nomes: Collection<String> = listOf("Matheus", "Fran", "Gui", "Maria", "Ana")
     for (nome in nomes) {
         println(nome)
@@ -9,5 +37,4 @@ fun main() {
     println(nomes)
     println("Tem o nome Matheus? ${nomes.contains("Matheus")}")
     println("Tamanho da coleção ${nomes.size}")
-
 }
