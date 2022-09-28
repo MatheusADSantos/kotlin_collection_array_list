4d5c7070fb0efbf1b85b859095a2fce26b19e362
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-28 00:42:07 -0300

    Abstrai em arquivos: Funções das listas; Lista de livros e Livros(lista com e sem nulo) Implementei ? no tipo de listaComNulos e usei o filterNotNull() pra filtrar os nulos na lista na hora de retornar

---
+++ src/main/kotlin/list/Livros.kt
@@ -0,0 +1,30 @@
+package list
+
+val livro1 = Livro(
+    titulo = "Grande Sertão: Veredas",
+    autor = "João Guimarães Rosa",
+    anoPublicacao = 1956
+)
+val livro2 = Livro(
+    titulo = "Minha vida de menina",
+    autor = "Helena Morley",
+    anoPublicacao = 1942,
+    editora = "Editora A"
+)
+val livro3 = Livro(
+    titulo = "Memórias Póstumas de Brás Cubas",
+    autor = "Machado de Assis",
+    anoPublicacao = 1881
+)
+val livro4 = Livro(
+    titulo = "Iracema",
+    autor = "José de Alencar",
+    anoPublicacao = 1865,
+    editora = "Editora B"
+)
+
+// Criando uma lista do tipo MutableList<Livro>
+val livros: MutableList<Livro?> = mutableListOf(livro1, livro2, livro3, livro4)
+
+// Criando uma lista do tipo MutableList<Livro?> por ser do tipo <Livro?> pode receber null...
+val livrosComNulos: MutableList<Livro?> = mutableListOf(livro1, null,  livro2, null, livro3, livro4, null)
