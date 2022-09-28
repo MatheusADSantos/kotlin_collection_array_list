9f3e2884a12f4d1246a69f85e897146f23996a40
MatheusADSantos <matheusdevmobile2020@gmail.com>
2022-09-28 17:47:55 -0300

    Set: Funções do set -> union, subtract, intersect ...

--- src/main/kotlin/MatheusADSantos/com/github/main.kt
+++ src/main/kotlin/MatheusADSantos/com/github/main.kt
@@ -10,6 +10,23 @@ fun main() {
     assistiramAmbas.addAll(assistiramCursoJava)
     assistiramAmbas.add("Maria")
     assistiramAmbas.add("Maria") // Não irá add outra Ana, pq o set só aceita elementos unicos
-    println(assistiramAmbas)
+//    println(assistiramAmbas)
 
+    // União(+) entre 2 set: Soma tudo
+    println(assistiramCursoJava + assistiramCursoKotlin)
+    println(assistiramCursoJava union assistiramCursoKotlin)
+
+    // Subtração(-) entre 2 set: Diferença entre elas
+    println(assistiramCursoJava - assistiramCursoKotlin)
+    println(assistiramCursoJava subtract assistiramCursoKotlin)
+
+    // Intersecção entre 2 set: O que tem em ambas
+    println(assistiramCursoJava intersect assistiramCursoKotlin)
+
+    // Fazendo uma cópia para MutableList, permitindo assim repetir os elementos
+    val assistiramAmbasList = assistiramAmbas.toMutableList()
+    assistiramAmbasList.add("Matheus")
+    println(assistiramAmbasList)
+    // Aqui converto pra Set novamente, printando os elementos unicos
+    println(assistiramAmbasList.toSet())
 }
